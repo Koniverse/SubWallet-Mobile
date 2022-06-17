67221b93022ca5289355e1d3f95046ed0369799c
lw-cdm <linhtm@cdmteck.com>
2022-06-17 11:50:17 +0700

    Update SeedPhraseArea component

---
+++ src/components/SeedPhraseArea.tsx
@@ -0,0 +1,60 @@
+import { StyleProp, View, ViewProps } from 'react-native';
+import React from 'react';
+import { ColorMap } from 'styles/color';
+import { SeedWord } from 'components/SeedWord';
+
+interface SeedPhraseAreaProps extends ViewProps {
+  onTapWord: (word: string) => void;
+  originWords: string[];
+  currentWords: string[];
+}
+
+function getWrapperStyle(style: StyleProp<any> = {}): StyleProp<any> {
+  return {
+    borderRadius: 5,
+    paddingHorizontal: 14,
+    paddingTop: 14,
+    paddingBottom: 14,
+    backgroundColor: ColorMap.dark2,
+    minHeight: 211,
+    ...style,
+  };
+}
+
+const innerViewStyle: StyleProp<any> = {
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+};
+
+const seedWordStyle = {
+  margin: 2,
+};
+
+export const SeedPhraseArea = (seedPhraseAreaProps: SeedPhraseAreaProps) => {
+  const { currentWords, originWords, onTapWord, style } = seedPhraseAreaProps;
+
+  const _onTapWord = (word: string) => {
+    return () => {
+      onTapWord(word);
+    };
+  };
+
+  const renderSeedWord = (word: string, index: number) => {
+    return (
+      <SeedWord
+        style={seedWordStyle}
+        backgroundColor={ColorMap.dark1}
+        key={word}
+        title={word}
+        isError={index !== originWords.indexOf(word)}
+        onPress={_onTapWord(word)}
+      />
+    );
+  };
+
+  return (
+    <View style={getWrapperStyle(style)}>
+      <View style={innerViewStyle}>{currentWords.map((word, index) => renderSeedWord(word, index))}</View>
+    </View>
+  );
+};
--- src/screens/CreateAccount/VerifySecretPhrase.tsx
+++ src/screens/CreateAccount/VerifySecretPhrase.tsx
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleProp, View } from 'react-native';
 import { SeedWord } from 'components/SeedWord';
 import { ColorMap } from 'styles/color';
 import { sharedStyles } from 'styles/sharedStyles';
+import { SeedPhraseArea } from 'components/SeedPhraseArea';
 
 const phraseAreaStyle: StyleProp<any> = {
   paddingLeft: 14,
   paddingRight: 14,
   flexDirection: 'row',
   flexWrap: 'wrap',
+  flex: 1,
 };
 
 const seedWordStyle = {
   margin: 2,
 };
 
-const renderSeedWord = (word: string, index: number) => {
-  return (
-    <SeedWord style={seedWordStyle} key={word} prefixText={`${index + 1}`.padStart(2, '0')} title={word} disabled />
-  );
-};
-
 export const VerifySecretPhrase = () => {
+  const [selectedWord, setSelectedWord] = useState<string[]>(['gadget', 'assist']);
   const phrase = 'gadget copy assist junior exhibit lazy educate brain used dust stay wink';
+  const seedWords: string[] = phrase.split(' ');
+
+  // todo: randomize seedWords
+
+  const onSelectWord = (word: string) => {
+    return () => {
+      const newSelectedWord: string[] = [...selectedWord];
+      newSelectedWord.push(word);
+      setSelectedWord(newSelectedWord);
+    };
+  };
+
+  const onUnSelectWord = (word: string) => {
+    const newSelectedWord: string[] = selectedWord.filter(w => w !== word);
+
+    setSelectedWord(newSelectedWord);
+  };
+
+  const renderSeedWord = (word: string) => {
+    return (
+      <SeedWord
+        style={seedWordStyle}
+        key={word}
+        title={word}
+        onPress={onSelectWord(word)}
+        isActivated={selectedWord.includes(word)}
+      />
+    );
+  };
 
   return (
     <View style={{ backgroundColor: ColorMap.dark1, flex: 1, ...sharedStyles.blockContent }}>
-      <View style={phraseAreaStyle}>{phrase.split(' ').map(renderSeedWord)}</View>
+      <SeedPhraseArea currentWords={selectedWord} onTapWord={onUnSelectWord} originWords={seedWords} />
+      <View style={phraseAreaStyle}>
+        {seedWords.map((word) => renderSeedWord(word))}
+      </View>
     </View>
   );
 };
