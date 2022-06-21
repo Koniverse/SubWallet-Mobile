bbdf7e214203c4570d3d8e95bc503879456dcb7f
lw-cdm <linhtm@cdmteck.com>
2022-06-21 09:22:54 +0700

    Fix first screen issue happens when reloading app

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@
 
 import React from 'react';
 import { WebViewProvider } from 'providers/WebViewProvider';
-import { persistor, store } from './stores';
-import { Provider } from 'react-redux';
+import { RootState } from './stores';
+import { useSelector } from 'react-redux';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { CreateAccount } from 'screens/CreateAccount';
@@ -13,7 +13,6 @@ import { ThemeContext } from 'providers/contexts';
 import { THEME_PRESET } from 'styles/themes';
 import { ToastProvider } from 'react-native-toast-notifications';
 import { AccountList } from 'screens/AccountList';
-import { PersistGate } from 'redux-persist/integration/react';
 import { QrScanner } from 'screens/QrScanner';
 import { QrScannerProvider } from 'providers/QrScannerProvider';
 import { RootStackParamList } from 'types/routes';
@@ -42,84 +41,72 @@ export const App = () => {
   StatusBar.setBarStyle(isDarkMode ? 'light-content' : 'dark-content');
   const {
     accounts: { accounts },
-  } = store.getState();
+  } = useSelector((state: RootState) => state);
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ToastProvider
-          placement="top"
-          normalColor={theme.colors.notification}
-          successColor={theme.colors.primary}
-          warningColor={theme.colors.notification_warning}
-          dangerColor={theme.colors.notification_danger}>
-          <WebViewProvider>
-            <QrScannerProvider navigationRef={navigationRef}>
-              <ThemeContext.Provider value={theme}>
-                <NavigationContainer ref={navigationRef} theme={theme}>
-                  <Stack.Navigator
-                    initialRouteName={accounts && accounts.length ? 'Home' : 'FirstScreen'}
-                    screenOptions={{
-                      animation: 'fade_from_bottom',
-                    }}>
-                    <Stack.Group screenOptions={{ headerShown: false }}>
-                      <Stack.Screen name="FirstScreen" component={FirstScreen} />
-                      <Stack.Screen name="Home" component={Home} />
-                      <Stack.Screen
-                        name="CreateAccount"
-                        component={CreateAccount}
-                        options={{ title: 'Create Account' }}
-                      />
-                      <Stack.Screen name="AccountList" component={AccountList} options={{ title: 'Account List' }} />
-                      <Stack.Screen
-                        name="AccountsScreen"
-                        component={AccountsScreen}
-                        options={{ title: 'Account Screen' }}
-                      />
-                      <Stack.Screen name="EditAccount" component={EditAccount} options={{ title: 'Edit Account' }} />
-                      <Stack.Screen name="RestoreJson" component={RestoreJson} options={{ title: 'Restore JSON' }} />
-                      <Stack.Screen
-                        name="ExportPrivateKey"
-                        component={ViewPrivateKey}
-                        options={{ title: 'Export Private Key' }}
-                      />
-                      <Stack.Screen name="Settings" component={Settings} options={{ title: 'Settings' }} />
-                      <Stack.Screen
-                        name="RemoveAccount"
-                        component={RemoveAccount}
-                        options={{ title: 'Remove Account' }}
-                      />
-                      {/*<Stack.Screen*/}
-                      {/*  name="NetworkSelect"*/}
-                      {/*  component={NetworkSelect}*/}
-                      {/*  options={{ title: 'Network Select' }}*/}
-                      {/*/>*/}
-                      <Stack.Screen
-                        name="NetworksSetting"
-                        component={NetworksSetting}
-                        options={{ title: 'Networks Setting' }}
-                      />
-                      <Stack.Screen
-                        name="ImportSecretPhrase"
-                        component={ImportSecretPhrase}
-                        options={{ title: 'Import Secret Phrase' }}
-                      />
-                    </Stack.Group>
-                    <Stack.Group
-                      screenOptions={{
-                        presentation: 'modal',
-                        headerShown: false,
-                      }}>
-                      <Stack.Screen name="QrScanner" component={QrScanner} />
-                    </Stack.Group>
-                  </Stack.Navigator>
-                </NavigationContainer>
-              </ThemeContext.Provider>
-            </QrScannerProvider>
-          </WebViewProvider>
-        </ToastProvider>
-      </PersistGate>
-    </Provider>
+    <ToastProvider
+      placement="top"
+      normalColor={theme.colors.notification}
+      successColor={theme.colors.primary}
+      warningColor={theme.colors.notification_warning}
+      dangerColor={theme.colors.notification_danger}>
+      <WebViewProvider>
+        <QrScannerProvider navigationRef={navigationRef}>
+          <ThemeContext.Provider value={theme}>
+            <NavigationContainer ref={navigationRef} theme={theme}>
+              <Stack.Navigator
+                initialRouteName={accounts && accounts.length ? 'Home' : 'FirstScreen'}
+                screenOptions={{
+                  animation: 'fade_from_bottom',
+                }}>
+                <Stack.Group screenOptions={{ headerShown: false }}>
+                  <Stack.Screen name="FirstScreen" component={FirstScreen} />
+                  <Stack.Screen name="Home" component={Home} />
+                  <Stack.Screen name="CreateAccount" component={CreateAccount} options={{ title: 'Create Account' }} />
+                  <Stack.Screen name="AccountList" component={AccountList} options={{ title: 'Account List' }} />
+                  <Stack.Screen
+                    name="AccountsScreen"
+                    component={AccountsScreen}
+                    options={{ title: 'Account Screen' }}
+                  />
+                  <Stack.Screen name="EditAccount" component={EditAccount} options={{ title: 'Edit Account' }} />
+                  <Stack.Screen name="RestoreJson" component={RestoreJson} options={{ title: 'Restore JSON' }} />
+                  <Stack.Screen
+                    name="ExportPrivateKey"
+                    component={ViewPrivateKey}
+                    options={{ title: 'Export Private Key' }}
+                  />
+                  <Stack.Screen name="Settings" component={Settings} options={{ title: 'Settings' }} />
+                  <Stack.Screen name="RemoveAccount" component={RemoveAccount} options={{ title: 'Remove Account' }} />
+                  {/*<Stack.Screen*/}
+                  {/*  name="NetworkSelect"*/}
+                  {/*  component={NetworkSelect}*/}
+                  {/*  options={{ title: 'Network Select' }}*/}
+                  {/*/>*/}
+                  <Stack.Screen
+                    name="NetworksSetting"
+                    component={NetworksSetting}
+                    options={{ title: 'Networks Setting' }}
+                  />
+                  <Stack.Screen
+                    name="ImportSecretPhrase"
+                    component={ImportSecretPhrase}
+                    options={{ title: 'Import Secret Phrase' }}
+                  />
+                </Stack.Group>
+                <Stack.Group
+                  screenOptions={{
+                    presentation: 'modal',
+                    headerShown: false,
+                  }}>
+                  <Stack.Screen name="QrScanner" component={QrScanner} />
+                </Stack.Group>
+              </Stack.Navigator>
+            </NavigationContainer>
+          </ThemeContext.Provider>
+        </QrScannerProvider>
+      </WebViewProvider>
+    </ToastProvider>
   );
 };
 
