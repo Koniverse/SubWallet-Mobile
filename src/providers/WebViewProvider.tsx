88a4114583d843845e65a65bf9df77bb21905976
lw-cdm <linhtm@cdmteck.com>
2022-07-22 15:47:17 +0700

    refactor code, remove react-native-static-server lib

--- src/providers/WebViewProvider.tsx
+++ src/providers/WebViewProvider.tsx
@@ -5,10 +5,6 @@ import { listenMessage, setViewRef } from '../messaging';
 import { NativeSyntheticEvent, Platform, View } from 'react-native';
 import { WebViewMessage } from 'react-native-webview/lib/WebViewTypes';
 import SplashScreen from 'react-native-splash-screen';
-// import * as RNFS from 'react-native-fs';
-// @ts-ignore
-// import StaticServer from '@dr.pogodin/react-native-static-server';
-
 interface WebViewProviderProps {
   children?: React.ReactNode;
   viewRef?: MutableRefObject<WebView | undefined>;
@@ -30,57 +26,13 @@ const ERROR_HANDLE_SCRIPT = `
     };
     true;
 `;
-// const getPath = () => {
-//   return Platform.OS === 'android' ? RNFS.DocumentDirectoryPath + '/www' : RNFS.MainBundlePath + '/www';
-// };
-
-// const moveAndroidFiles = async () => {
-//   if (Platform.OS === 'android') {
-//     await RNFS.mkdir(RNFS.DocumentDirectoryPath + '/www');
-//     const files = ['www/index.html', 'www/index.css', 'www/index.js'];
-//     for (const file of files) {
-//       await RNFS.copyFileAssets(file, RNFS.DocumentDirectoryPath + '/' + file);
-//     }
-//   }
-// };
 
 export const WebViewProvider = ({ children }: WebViewProviderProps): React.ReactElement<WebViewProviderProps> => {
   const webRef = useRef<WebView>();
-  // let path = getPath();
-  // const [webViewUrl, setWebViewUrl] = useState<string>('');
-  // const [server] = useState(new StaticServer(11223, path));
   const sourceUri = (Platform.OS === 'android' ? 'file:///android_asset/' : '') + 'Web.bundle/loader.html';
   const [status, setStatus] = useState('init');
 
-  // useEffect(() => {
-  //   let isSync = true;
-  //   console.log('---- Init server');
-  //
-  //   server.start().then((serverUrl: string) => {
-  //     console.log(serverUrl);
-  //     if (isSync) {
-  //       if (Platform.OS === 'ios') {
-  //         setWebViewUrl(`${serverUrl}/index.html`);
-  //       } else {
-  //         setWebViewUrl('file:///android_asset/web-runner-core/index.html');
-  //       }
-  //     }
-  //   });
-  //
-  //   return () => {
-  //     console.log('---- End server');
-  //     if (server && server.isRunning()) {
-  //       server.stop();
-  //     }
-  //     isSync = false;
-  //   };
-  // }, [server]);
-
   const onMessage = useCallback((data: NativeSyntheticEvent<WebViewMessage>) => {
-    // if (!webViewUrl) {
-    //   return;
-    // }
-
     // eslint-disable-next-line @typescript-eslint/no-shadow
     listenMessage(JSON.parse(data.nativeEvent.data), data => {
       // @ts-ignore
