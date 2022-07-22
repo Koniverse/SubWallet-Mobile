c44c2fac10c6dd38c91f5ea0019fb4c57afba423
AnhMTV <maithachvietanh@gmail.com>
2022-07-22 18:48:42 +0700

    Add sentry report to SubWallet Mobile

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,17 @@
+import * as Sentry from "@sentry/react-native";
 import React from 'react';
 import { persistor, store } from 'stores/index';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import App from './App';
 import { WebViewProvider } from 'providers/WebViewProvider';
+
+Sentry.init({
+  dsn: "https://6745bba7df994e919effe7b2ba765101@o1330554.ingest.sentry.io/6593484",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
 
 export const Root = () => {
   return (
@@ -17,4 +25,4 @@ export const Root = () => {
   );
 };
 
-export default Root;
+export default Sentry.wrap(Root);
