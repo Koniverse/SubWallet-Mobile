7c0f2ef34e8fcca152b71ce55bd533cca9a930c8
quangdo <quangdm@cdmteck.com>
2022-07-27 08:55:12 +0700

    update Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/react-native";
+import * as Sentry from '@sentry/react-native';
 import React from 'react';
 import { persistor, store } from 'stores/index';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -7,7 +7,7 @@ import App from './App';
 import { WebViewProvider } from 'providers/WebViewProvider';
 
 Sentry.init({
-  dsn: 'https://o1330554.ingest.sentry.io/6593484',
+  dsn: 'https://6745bba7df994e919effe7b2ba765101@o1330554.ingest.sentry.io/6593484',
   environment: 'production',
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
   // We recommend adjusting this value in production.
