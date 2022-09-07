74c2535c073f9e22343a731d8da195e981667e76
quangdo <quangdm@cdmteck.com>
2022-09-07 18:39:49 +0700

    Update Authorize Index for Authorize

--- src/screens/Home/Browser/ConfirmationPopup/index.tsx
+++ src/screens/Home/Browser/ConfirmationPopup/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { StyleProp, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleProp, Text, View } from 'react-native';
 import { ColorMap } from 'styles/color';
 import { AuthorizeRequest } from 'screens/Home/Browser/ConfirmationPopup/AuthorizeRequest';
 import { getHostName } from 'utils/browser';
+import { IconButton } from 'components/IconButton';
+import { ArrowLeft, ArrowRight } from 'phosphor-react-native';
+import useConfirmations from 'hooks/useConfirmations';
+import { FontMedium, sharedStyles } from 'styles/sharedStyles';
 
 const subWalletModalSeparator: StyleProp<any> = {
   width: 56,
   height: 4,
   borderRadius: 2,
   backgroundColor: 'rgba(255, 255, 255, 0.1)',
-  marginBottom: 19,
 };
 
 const confirmationPopupWrapper: StyleProp<any> = {
@@ -23,13 +26,47 @@ const confirmationPopupWrapper: StyleProp<any> = {
   paddingHorizontal: 16,
 };
 
+const confirmationHeader: StyleProp<any> = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  width: '100%',
+  alignItems: 'center',
+  marginTop: 8,
+  marginBottom: 16,
+};
+
+const authorizeIndexTextStyle: StyleProp<any> = { ...sharedStyles.mainText, ...FontMedium, color: ColorMap.light };
+
+const requestListLength = 3;
+
 export const ConfirmationPopup = () => {
   const url = 'https://portal.astar.network';
   const hostName = getHostName(url);
+  const {} = useConfirmations();
+  const [confirmationIndex, setConfirmationIndex] = useState<number>(1);
+  const onPressPrevButton = () => {
+    if (confirmationIndex > 1) {
+      setConfirmationIndex(confirmationIndex - 1);
+    }
+  };
+
+  const onPressNextButton = () => {
+    if (confirmationIndex < requestListLength) {
+      setConfirmationIndex(confirmationIndex + 1);
+    }
+  };
+
   return (
     <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end' }}>
       <View style={confirmationPopupWrapper}>
         <View style={subWalletModalSeparator} />
+        <View style={confirmationHeader}>
+          <IconButton icon={ArrowLeft} color={ColorMap.disabled} onPress={onPressPrevButton} />
+          <Text style={authorizeIndexTextStyle}>
+            <Text>{confirmationIndex}</Text>/<Text>{requestListLength}</Text>
+          </Text>
+          <IconButton icon={ArrowRight} color={ColorMap.disabled} onPress={onPressNextButton} />
+        </View>
         <AuthorizeRequest request={{ origin: hostName }} />
       </View>
     </View>
