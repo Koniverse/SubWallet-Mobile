dd5ed72e5cfb7b9e77f41df3ccac4e8f8d5de5b5
quangdo <quangdm@cdmteck.com>
2022-09-16 15:19:28 +0700

    Setup Deep-link for iOS

--- ios/SubWalletMobile/AppDelegate.mm
+++ ios/SubWalletMobile/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <React/RCTLinkingManager.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -104,5 +104,12 @@
 }
 
 #endif
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 
 @end
