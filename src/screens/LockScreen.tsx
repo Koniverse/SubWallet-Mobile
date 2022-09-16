31a59d998d348b32631a34d1746d952940cd5969
lw-cdm <linhtm@cdmteck.com>
2022-09-16 14:43:30 +0700

    Setup deeplink for dapp browser

--- src/screens/LockScreen.tsx
+++ src/screens/LockScreen.tsx
@@ -9,8 +9,6 @@ import i18n from 'utils/i18n/i18n';
 import { useBlurOnFulfill } from 'react-native-confirmation-code-field';
 import { CELL_COUNT } from '../constant';
 import useAppLock from 'hooks/useAppLock';
-import { NavigationProps } from 'types/routes';
-import { useIsFocused } from '@react-navigation/native';
 import TouchID from 'react-native-touch-id';
 import { useSelector } from 'react-redux';
 import { RootState } from 'stores/index';
@@ -27,7 +25,7 @@ const optionalConfigObject = {
   passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
 };
 
-export const LockScreen = ({ navigation }: NavigationProps) => {
+export const LockScreen = () => {
   const { unlock } = useAppLock();
   const faceIdEnabled = useSelector((state: RootState) => state.mobileSettings.faceIdEnabled);
   const [value, setValue] = useState<string>('');
@@ -36,30 +34,28 @@ export const LockScreen = ({ navigation }: NavigationProps) => {
   const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
 
   const unlockWithBiometric = useAppLock().unlockWithBiometric;
-  const isFocused = useIsFocused();
 
   useEffect(() => {
     const _authMethod = faceIdEnabled ? 'biometric' : 'pinCode';
-    if (isFocused) {
-      if (_authMethod === 'biometric') {
-        TouchID.isSupported()
-          .then(currentType => {
-            TouchID.authenticate(`Sign in with ${currentType}`, optionalConfigObject)
-              .then(() => {
-                unlockWithBiometric();
-              })
-              .catch(() => {
-                setAuthMethod('pinCode');
-              });
-          })
-          .catch(() => setAuthMethod('pinCode'));
-      } else {
-        ref.current?.focus();
-      }
+    if (_authMethod === 'biometric') {
+      TouchID.isSupported()
+        .then(currentType => {
+          TouchID.authenticate(`Sign in with ${currentType}`, optionalConfigObject)
+            .then(() => {
+              unlockWithBiometric();
+            })
+            .catch(() => {
+              setAuthMethod('pinCode');
+            });
+        })
+        .catch(() => setAuthMethod('pinCode'));
+    } else {
+      ref.current?.focus();
     }
+
     setAuthMethod(_authMethod);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [faceIdEnabled, isFocused, navigation, unlockWithBiometric]);
+  }, [faceIdEnabled, unlockWithBiometric]);
 
   useEffect(() => {
     if (value.length === 6) {
@@ -71,7 +67,7 @@ export const LockScreen = ({ navigation }: NavigationProps) => {
     } else {
       setError('');
     }
-  }, [navigation, unlock, value]);
+  }, [unlock, value]);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
