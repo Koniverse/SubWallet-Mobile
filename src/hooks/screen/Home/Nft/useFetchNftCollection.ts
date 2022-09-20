e98e1c257469ab674c8b0382dc71b059e540b8cf
AnhMTV <maithachvietanh@gmail.com>
2022-09-20 18:11:20 +0700

    Remove address compare check for NFT until have better solution to compare 2 address with diffirent format

--- src/hooks/screen/Home/Nft/useFetchNftCollection.ts
+++ src/hooks/screen/Home/Nft/useFetchNftCollection.ts
@@ -8,35 +8,29 @@ export default function useFetchNftCollection(): NftCollectionType {
   const nftCollectionList = useSelector((state: RootState) => state.nftCollection.nftCollectionList);
   const nftList = useSelector((state: RootState) => state.nft.nftList);
   const networkMap = useSelector((state: RootState) => state.networkMap.details);
-  const accounts = useSelector((state: RootState) => state.accounts.accounts);
 
   return useMemo((): NftCollectionType => {
     const networkList = Object.keys(networkMap);
-    const accountList = accounts.map(account => account.address);
     const nftCollections: NftCollection[] = [];
     for (const nftCollection of nftCollectionList) {
       if (!networkList.includes(nftCollection.chain || '')) {
         continue;
       }
-      const collection: NftCollection = { ...nftCollection };
+      const tmp: NftCollection = { ...nftCollection };
       let count = 0;
       for (const nft of nftList) {
-        if (
-          nft.chain === nftCollection.chain &&
-          nft.collectionId === nftCollection.collectionId &&
-          accountList.includes(nft.owner || '')
-        ) {
+        if (nft.chain === nftCollection.chain && nft.collectionId === nftCollection.collectionId) {
           count++;
         }
       }
       if (count > 0) {
-        collection.itemCount = count;
-        nftCollections.push(collection);
+        tmp.itemCount = count;
+        nftCollections.push(tmp);
       }
     }
 
     return {
       nftCollections,
     };
-  }, [accounts, networkMap, nftCollectionList, nftList]);
+  }, [networkMap, nftCollectionList, nftList]);
 }
