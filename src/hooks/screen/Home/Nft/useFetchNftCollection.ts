3bc641f14a06daa62df322185a06e4e924e91d28
AnhMTV <maithachvietanh@gmail.com>
2022-09-20 17:24:00 +0700

    Fix error still show NFT when forget account or turn off network

--- src/hooks/screen/Home/Nft/useFetchNftCollection.ts
+++ src/hooks/screen/Home/Nft/useFetchNftCollection.ts
@@ -7,23 +7,36 @@ import { RootState } from 'stores/index';
 export default function useFetchNftCollection(): NftCollectionType {
   const nftCollectionList = useSelector((state: RootState) => state.nftCollection.nftCollectionList);
   const nftList = useSelector((state: RootState) => state.nft.nftList);
+  const networkMap = useSelector((state: RootState) => state.networkMap.details);
+  const accounts = useSelector((state: RootState) => state.accounts.accounts);
 
   return useMemo((): NftCollectionType => {
+    const networkList = Object.keys(networkMap);
+    const accountList = accounts.map(account => account.address);
     const nftCollections: NftCollection[] = [];
     for (const nftCollection of nftCollectionList) {
-      const tmp: NftCollection = { ...nftCollection };
+      if (!networkList.includes(nftCollection.chain || '')) {
+        continue;
+      }
+      const collection: NftCollection = { ...nftCollection };
       let count = 0;
       for (const nft of nftList) {
-        if (nft.chain === nftCollection.chain && nft.collectionId === nftCollection.collectionId) {
+        if (
+          nft.chain === nftCollection.chain &&
+          nft.collectionId === nftCollection.collectionId &&
+          accountList.includes(nft.owner || '')
+        ) {
           count++;
         }
       }
-      tmp.itemCount = count;
-      nftCollections.push(tmp);
+      if (count > 0) {
+        collection.itemCount = count;
+        nftCollections.push(collection);
+      }
     }
 
     return {
       nftCollections,
     };
-  }, [nftCollectionList, nftList]);
+  }, [accounts, networkMap, nftCollectionList, nftList]);
 }
