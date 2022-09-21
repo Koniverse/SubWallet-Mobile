851b7ffcca1d7a84cb9827c2b530f0daa3d3f447
AnhMTV <maithachvietanh@gmail.com>
2022-09-21 09:52:47 +0700

    Optimize useFetchNftCollections

--- src/hooks/screen/Home/Nft/useFetchNftCollection.ts
+++ src/hooks/screen/Home/Nft/useFetchNftCollection.ts
@@ -4,35 +4,39 @@ import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'stores/index';
 import reformatAddress from 'utils/index';
+import {ALL_ACCOUNT_KEY} from "@subwallet/extension-koni-base/constants";
 
 export default function useFetchNftCollection(): NftCollectionType {
   const nftCollectionList = useSelector((state: RootState) => state.nftCollection.nftCollectionList);
   const nftList = useSelector((state: RootState) => state.nft.nftList);
   const networkMap = useSelector((state: RootState) => state.networkMap.details);
-  const accounts = useSelector((state: RootState) => state.accounts.accounts);
+  const currentAccountAddress = useSelector((state: RootState) => state.accounts.currentAccountAddress);
   const networkList = Object.keys(networkMap);
-  const accountList = accounts.map(account => account.address);
 
   return useMemo((): NftCollectionType => {
     const nftCollections: NftCollection[] = [];
+    const countMap: Record<string, number> = {};
+
+    for (const nft of nftList) {
+      const owner = nft.owner ? reformatAddress(nft.owner, 42, false) : '';
+      if (currentAccountAddress === ALL_ACCOUNT_KEY || currentAccountAddress === owner) {
+        const key = `${nft.chain}-${nft.collectionId}`;
+        if (countMap.hasOwnProperty(key)) {
+          countMap[key] = countMap[key] + 1;
+        } else {
+          countMap[key] = 1;
+        }
+      }
+    }
+
     for (const nftCollection of nftCollectionList) {
       if (!networkList.includes(nftCollection.chain || '')) {
         continue;
       }
       const collection: NftCollection = { ...nftCollection };
-      let count = 0;
-      for (const nft of nftList) {
-        const owner = nft.owner ? reformatAddress(nft.owner, 42, false) : '';
-        if (
-          nft.chain === nftCollection.chain &&
-          nft.collectionId === nftCollection.collectionId &&
-          accountList.includes(owner)
-        ) {
-          count++;
-        }
-      }
-      if (count > 0) {
-        collection.itemCount = count;
+      const key = `${collection.chain}-${collection.collectionId}`;
+      if (countMap[key] && countMap[key] > 0) {
+        collection.itemCount = countMap[key];
         nftCollections.push(collection);
       }
     }
@@ -41,5 +45,5 @@ export default function useFetchNftCollection(): NftCollectionType {
       nftCollections,
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(accountList), JSON.stringify(networkList), nftCollectionList, nftList]);
+  }, [currentAccountAddress, JSON.stringify(networkList), nftCollectionList, nftList]);
 }
