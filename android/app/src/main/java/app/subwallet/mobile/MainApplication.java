b980d739c795673f6776933f7fbc26a430f56a42
AnhMTV <maithachvietanh@gmail.com>
2022-10-04 17:00:13 +0700

    Update folder same as new app package name

--- android/app/src/main/java/app/subwallet/mobile/MainApplication.java
+++ android/app/src/main/java/app/subwallet/mobile/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -75,7 +73,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("app.subwallet.mobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("app.subwall.mobile.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
