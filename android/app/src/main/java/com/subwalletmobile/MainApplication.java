e2b2d2f186ebc4f7b247f042fc5a29383101dcca
AnhMTV <maithachvietanh@gmail.com>
2022-10-04 14:52:47 +0700

    Change package name from com.subwalletmobile to app.subwallet.mobile

--- android/app/src/main/java/com/subwalletmobile/MainApplication.java
+++ android/app/src/main/java/com/subwalletmobile/MainApplication.java
@@ -1,4 +1,4 @@
-package com.subwalletmobile;
+package app.subwallet.mobile;
 
 import android.app.Application;
 import android.content.Context;
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import com.subwalletmobile.newarchitecture.MainApplicationReactNativeHost;
+import app.subwallet.mobile.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -75,7 +75,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.subwalletmobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("app.subwallet.mobile.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- android/app/src/main/java/com/subwalletmobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/subwalletmobile/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.subwalletmobile.newarchitecture.modules;
+package app.subwallet.mobile.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
