fe2ce9dec5a01e31222e74e963acd006e6f469a8
Thức Nguyễn Duy <thucnguyenduy@Thucs-Mac-mini.Home>
2023-04-11 16:14:01 +0700

    [issue-407] add error fallback screen

---
+++ src/components/common/ErrorFallbackScreen/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Button, Icon, Typography } from '../../design-system-ui';
+import LinearGradient from 'react-native-linear-gradient';
+import FastImage from 'react-native-fast-image';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { House } from 'phosphor-react-native';
+import ErrorStyles from './style';
+import { useSubWalletTheme } from 'hooks/useSubWalletTheme';
+
+interface ErrorFallbackProps {
+  resetError: () => void;
+}
+
+const ErrorFallbackScreen: React.FC<ErrorFallbackProps> = ({ resetError }) => {
+  const theme = useSubWalletTheme().swThemes;
+  const styles = ErrorStyles(theme);
+  return (
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+        locations={[0, 0.5]}
+        colors={['rgba(234, 76, 76, 0.15)', 'rgba(217, 217, 217, 0)']}
+        style={styles.gradientContainer}>
+        <View style={styles.content}>
+          <FastImage source={require('../../../assets/circle-robot.png')} style={styles.logo} />
+          <Typography.Title style={styles.title} level={3}>
+            Opps! An Error Occurred
+          </Typography.Title>
+          <Typography.Text size="lg" style={styles.textContent}>
+            Sorry, something went wrong. Please try again later.
+          </Typography.Text>
+        </View>
+        <Button onPress={() => resetError()} icon={<Icon phosphorIcon={House} size="md" weight="fill" />}>
+          Back to home
+        </Button>
+      </LinearGradient>
+    </SafeAreaView>
+  );
+};
+
+export default ErrorFallbackScreen;
---
+++ src/components/common/ErrorFallbackScreen/style/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
+import { ImageStyle } from 'react-native-fast-image';
+import { ThemeTypes } from 'styles/themes';
+export interface ButtonStyles {
+  container: ViewStyle;
+  gradientContainer: ViewStyle;
+  content: ViewStyle;
+  logo: ImageStyle;
+  title: TextStyle;
+  textContent: TextStyle;
+}
+
+export default (theme: ThemeTypes) =>
+  StyleSheet.create<ButtonStyles>({
+    container: { flex: 1, backgroundColor: 'black' },
+    gradientContainer: { flex: 1, paddingBottom: theme.paddingXL },
+    content: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    logo: { width: 112, height: 112, marginBottom: 32 },
+    title: { marginBottom: theme.margin, color: '#BF1616', fontWeight: `${theme.fontWeightStrong}` },
+    textContent: { paddingHorizontal: 40, textAlign: 'center', color: 'rgba(255, 255, 255, 0.65)' },
+  });
