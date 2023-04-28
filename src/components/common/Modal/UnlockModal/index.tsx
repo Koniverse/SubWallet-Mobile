a3c38d670e230f4435464254988744e892e91782
S2kael <laiducminh1002@gmail.com>
2023-04-28 15:27:59 +0700

    [Issue-502] merge effect

--- src/components/common/Modal/UnlockModal/index.tsx
+++ src/components/common/Modal/UnlockModal/index.tsx
@@ -73,16 +73,14 @@ export const UnlockModal: React.FC<Props> = (props: Props) => {
   );
 
   useEffect(() => {
-    focus('password')();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
     if (!visible) {
       onChangeValue('password')('');
       onUpdateErrors('password')([]);
+    } else {
+      focus('password')();
     }
-  }, [visible, onChangeValue, onUpdateErrors]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [visible]);
 
   return (
     <SwModal
