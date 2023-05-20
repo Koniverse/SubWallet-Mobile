6d8106e9496606fc27d3cb54e6343c5f89ff640a
AnhMTV <maithachvietanh@gmail.com>
2023-05-20 22:07:14 +0700

    Release version 1.0.3 (156) [2]

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -5,6 +5,6 @@ cd ./android
 ./gradlew clean && ./gradlew bundleRelease
 
 # Build apk from the bundle follow this instruction https://stackoverflow.com/questions/53040047/generate-apk-file-from-aab-file-android-app-bundle
-java -jar /Volumes/MacData/App/bundletool.jar build-apks --bundle=$PWD/app/build/outputs/bundle/release/app-release.aab --output=$PWD/app/build/outputs/apk/build.apks --mode=universal --ks=/Volumes/MacData/App/my-upload-key.keystore --ks-pass=pass:$KS_PASSWORD --ks-key-alias=my-key-alias --key-pass=pass:$KS_PASSWORD
-
-unzip -p $PWD/app/build/outputs/apk/build.apks universal.apk > $PWD/app/build/outputs/apk/universal.apk
+#java -jar /Volumes/MacData/App/bundletool.jar build-apks --bundle=$PWD/app/build/outputs/bundle/release/app-release.aab --output=$PWD/app/build/outputs/apk/build.apks --mode=universal --ks=/Volumes/MacData/App/my-upload-key.keystore --ks-pass=pass:$KS_PASSWORD --ks-key-alias=my-key-alias --key-pass=pass:$KS_PASSWORD
+#
+#unzip -p $PWD/app/build/outputs/apk/build.apks universal.apk > $PWD/app/build/outputs/apk/universal.apk
