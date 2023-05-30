cf7b5b3707d43c27e2ff42a80785034bb34a4350
AnhMTV <maithachvietanh@gmail.com>
2023-05-30 19:01:06 +0700

    Allow to change web-runner url from Debug Screen

--- src/screens/WebViewDebugger.tsx
+++ src/screens/WebViewDebugger.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { ContainerWithSubHeader } from 'components/ContainerWithSubHeader';
 import { useNavigation } from '@react-navigation/native';
@@ -6,99 +6,53 @@ import { RootNavigationProps } from 'routes/index';
 import i18n from 'utils/i18n/i18n';
 import { sharedStyles } from 'styles/sharedStyles';
 import { WebRunnerContext } from 'providers/contexts';
-import { Button } from 'components/Button';
-import { MessageTypes } from '@subwallet/extension-base/background/types';
-import { Textarea } from 'components/Textarea';
-import { sendMessage } from 'messaging/index';
 import { useSubWalletTheme } from 'hooks/useSubWalletTheme';
-import { Dropdown } from 'components/Dropdown';
-
-// @ts-ignore
-const METHOD_MAP: Record<MessageTypes, { default_input: string; subscription?: boolean }> = {
-  'pri(accounts.subscribeWithCurrentAddress)': {
-    default_input: '{}',
-    subscription: true,
-  },
-  'pri(price.getPrice)': {
-    default_input: 'null',
-  },
-  'pri(price.getSubscription)': {
-    default_input: 'null',
-    subscription: true,
-  },
-  'pri(balance.getBalance)': {
-    default_input: 'null',
-  },
-  'pri(balance.getSubscription)': {
-    default_input: 'null',
-    subscription: true,
-  },
-  'pri(accounts.create.external)': {
-    default_input: '{"address": "address", "genesisHash": "", "name": "External Account 01"}',
-  },
-};
-
-let unsub: (() => void) | undefined;
+import { Input } from 'react-native-elements';
+import { mmkvStore } from 'utils/storage';
+import { AddressScanner } from 'components/Scanner/AddressScanner';
+import { Button } from 'components/design-system-ui';
 
 export const WebViewDebugger = () => {
   const navigation = useNavigation<RootNavigationProps>();
   const { webState, reload } = useContext(WebRunnerContext);
-  const [method, setMethod] = useState<MessageTypes>('pri(accounts.subscribeWithCurrentAddress)');
-  const [input, setInput] = useState(METHOD_MAP['pri(accounts.subscribeWithCurrentAddress)'].default_input);
-  const [result, setResult] = useState('');
-  const [error, setError] = useState('');
+  const [input, setInput] = useState('');
+  const [notification, setNotification] = useState('');
   const themeColors = useSubWalletTheme().colors;
 
+  const [showQr, setShowQr] = useState(false);
   const containerStyle = { marginBottom: 30 };
   const textStyle = { marginBottom: 10, color: themeColors.textColor };
 
-  const onPressBack = () => {
+  const onPressBack = useCallback(() => {
     navigation.goBack();
-  };
+  }, [navigation]);
 
-  const onPressReload = () => {
-    reload && reload();
-  };
+  const onPressReload = useCallback(() => {
+    reload();
+  }, [reload]);
 
-  const onChangeMethod = (value: MessageTypes) => {
-    setMethod(value);
-    setInput(METHOD_MAP[value].default_input);
-  };
+  const openQr = useCallback(async () => {
+    setShowQr(true);
+  }, []);
 
-  const onChangeInput = (value: string) => {
-    setInput(value);
-  };
+  const updateWebRunnerUrl = () => {
+    let url = input?.trim();
+    if (url) {
+      if (url.endsWith('/')) {
+        url = url.slice(0, -1);
+      }
 
-  const showRs = (rs: unknown) => {
-    setResult(JSON.stringify(rs, null, 2));
-  };
+      mmkvStore.set('__development_web_runner_url__', url);
 
-  const onSendMessage = () => {
-    const { subscription } = METHOD_MAP[method];
-    let callback;
-    if (subscription) {
-      let action: ((rs: unknown) => void) | undefined = (rs: unknown) => {
-        setResult(JSON.stringify(rs, null, 2));
-      };
-      callback = (rs: unknown) => {
-        action && action(rs);
-      };
-      unsub = () => {
-        console.log('Unsub');
-        action = undefined;
-      };
-    } else {
-      unsub = undefined;
+      setNotification("OK, Let's restart app!");
     }
+  };
 
-    // @ts-ignore
-    sendMessage(method, JSON.parse(input), callback)
-      .then(rs => {
-        showRs(rs);
-      })
-      .catch(err => {
-        setError(JSON.stringify(err));
-      });
+  const useDefaultWebRunner = () => {
+    setInput('');
+    mmkvStore.delete('__development_web_runner_url__');
+
+    setNotification("OK, Let's restart app!");
   };
 
   return (
@@ -108,21 +62,27 @@ export const WebViewDebugger = () => {
           <Text style={textStyle}>{`${i18n.common.status}${webState.status}`}</Text>
           <Text style={textStyle}>{`${i18n.common.url}${webState.url}`}</Text>
           <Text style={textStyle}>{`${i18n.common.version}${webState.version}`}</Text>
-          <Button title={i18n.common.reloadBackground} onPress={onPressReload} />
+          <Button onPress={onPressReload}>{i18n.common.reloadBackground}</Button>
         </View>
         <View style={containerStyle}>
-          <Text style={textStyle}>{i18n.common.message}</Text>
-          <Dropdown
-            onValueChange={onChangeMethod}
-            value={method}
-            items={Object.keys(METHOD_MAP).map(k => ({ label: k, value: k }))}
+          <Input value={input} onChangeText={setInput} style={textStyle} />
+          <AddressScanner
+            qrModalVisible={showQr}
+            onPressCancel={() => setShowQr(false)}
+            onChangeAddress={setInput}
+            networkKey={'default'}
+            scanMessage={'Scan URL'}
           />
-          <Text style={textStyle}>{i18n.common.input}</Text>
-          <Textarea value={input} style={{ ...textStyle, height: 80 }} onChangeText={onChangeInput} />
-          <Button style={{ marginBottom: 5 }} title={i18n.common.sendMessage} onPress={onSendMessage} />
-          {unsub && <Text style={{ textAlign: 'center', ...textStyle }}>{i18n.common.subscribing}</Text>}
-          <Text style={{ color: 'red' }}>{error}</Text>
-          <Text style={textStyle}>{result}</Text>
+          <Button style={{ marginBottom: 5 }} onPress={openQr}>
+            Scan
+          </Button>
+          <Button style={{ marginBottom: 5 }} onPress={updateWebRunnerUrl}>
+            Update Web Runner URL
+          </Button>
+          <Button style={{ marginBottom: 5 }} onPress={useDefaultWebRunner}>
+            Use Default
+          </Button>
+          <Text style={textStyle}>{notification}</Text>
         </View>
       </ScrollView>
     </ContainerWithSubHeader>
