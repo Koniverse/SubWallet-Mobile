53e471f7462d70018c744a4414a9116267e9977a
dominhquang <quangdm@subwallet.app>
2023-05-30 11:56:00 +0700

    [Issue-683]: Improve touchable area (Input Text)

--- src/components/Input/InputText.tsx
+++ src/components/Input/InputText.tsx
@@ -1,12 +1,13 @@
-import React, { forwardRef } from 'react';
-import { StyleProp, TextInput, View } from 'react-native';
+import React, { forwardRef, useMemo } from 'react';
+import { Platform, StyleSheet, TextInput, View, ViewStyle } from 'react-native';
 import { ColorMap } from 'styles/color';
-import { FontMedium, FontSize2 } from 'styles/sharedStyles';
-import { FieldBase, FieldBaseProps } from 'components/Field/Base';
+import { DisabledStyle, FontMedium } from 'styles/sharedStyles';
+import { FieldBaseProps } from 'components/Field/Base';
 import { Warning } from 'components/Warning';
-import { Icon } from 'components/design-system-ui';
+import { Icon, Typography } from 'components/design-system-ui';
 import { IconProps } from 'phosphor-react-native';
 import { useSubWalletTheme } from 'hooks/useSubWalletTheme';
+import { ThemeTypes } from 'styles/themes';
 
 interface Props extends FieldBaseProps {
   onChangeText?: (text: string) => void;
@@ -21,26 +22,8 @@ interface Props extends FieldBaseProps {
   placeholder?: string;
   leftIcon?: React.ElementType<IconProps>;
   rightIcon?: React.ReactNode;
-}
-
-const blockContentStyle: StyleProp<any> = {
-  flexDirection: 'row',
-  alignItems: 'center',
-  paddingHorizontal: 16,
-  paddingBottom: 12,
-};
-
-function getInputStyle(isError: boolean) {
-  return {
-    ...FontSize2,
-    paddingTop: 0,
-    paddingBottom: 0,
-    height: 25,
-    flex: 1,
-    paddingRight: 16,
-    ...FontMedium,
-    color: isError ? ColorMap.danger : ColorMap.light,
-  };
+  disabled?: boolean;
+  containerStyle?: ViewStyle;
 }
 
 const InputText = forwardRef((passwordFieldProps: Props, ref: React.Ref<TextInput>) => {
@@ -59,15 +42,29 @@ const InputText = forwardRef((passwordFieldProps: Props, ref: React.Ref<TextInpu
     placeholder,
     leftIcon,
     rightIcon,
-    ...fieldBase
+    disabled,
+    containerStyle,
   } = passwordFieldProps;
+  const hasLabel = !!label;
+  const styles = useMemo(
+    () => createStyle(theme, hasLabel, !!(errorMessages && errorMessages.length), !!leftIcon),
+    [theme, hasLabel, errorMessages, leftIcon],
+  );
+
   return (
     <>
-      <FieldBase label={label} {...fieldBase}>
-        <View style={[blockContentStyle, !label && { paddingTop: 12 }]}>
+      <View
+        style={[
+          styles.inputContainer,
+          containerStyle,
+          { backgroundColor: theme.colorBgSecondary },
+          disabled && DisabledStyle,
+        ]}>
+        {hasLabel && <Typography.Text style={styles.inputLabel}>{label}</Typography.Text>}
+        <>
           {leftIcon && (
-            <View style={{ paddingRight: theme.paddingXS }}>
-              <Icon phosphorIcon={leftIcon} iconColor={theme.colorTextLight5} />
+            <View style={{ position: 'absolute', top: (hasLabel ? 26 : 0) + 12, left: 12 }}>
+              <Icon size={'md'} phosphorIcon={leftIcon} iconColor={theme.colorTextLight5} />
             </View>
           )}
           <TextInput
@@ -76,7 +73,7 @@ const InputText = forwardRef((passwordFieldProps: Props, ref: React.Ref<TextInpu
             autoCapitalize={'none'}
             placeholder={placeholder}
             autoFocus={autoFocus}
-            style={getInputStyle(!!(errorMessages && errorMessages.length))}
+            style={styles.textInput}
             placeholderTextColor={ColorMap.disabled}
             selectionColor={ColorMap.disabled}
             blurOnSubmit={false}
@@ -90,8 +87,8 @@ const InputText = forwardRef((passwordFieldProps: Props, ref: React.Ref<TextInpu
             value={value}
           />
           {rightIcon}
-        </View>
-      </FieldBase>
+        </>
+      </View>
 
       {!!(errorMessages && errorMessages.length) &&
         errorMessages.map((message, index) => (
@@ -100,5 +97,44 @@ const InputText = forwardRef((passwordFieldProps: Props, ref: React.Ref<TextInpu
     </>
   );
 });
+
+function createStyle(theme: ThemeTypes, hasLabel: boolean, isValid: boolean, leftIcon: boolean, readonly?: boolean) {
+  return StyleSheet.create({
+    inputContainer: {
+      borderRadius: theme.borderRadiusLG,
+      backgroundColor: theme.colorBgSecondary,
+      width: '100%',
+      position: 'relative',
+      marginBottom: 8,
+      height: hasLabel ? 72 : 48,
+    },
+    inputLabel: {
+      ...FontMedium,
+      fontSize: theme.fontSizeSM,
+      paddingTop: theme.paddingXS,
+      lineHeight: theme.fontSizeSM * theme.lineHeightSM,
+      paddingHorizontal: theme.sizeSM,
+      color: theme.colorTextLight4,
+    },
+    textInput: {
+      ...FontMedium,
+      position: 'absolute',
+      flex: 1,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      paddingLeft: leftIcon ? theme.sizeSM + 32 : theme.sizeSM,
+      paddingRight: 14,
+      paddingTop: (hasLabel ? 26 : 0) + 14,
+      paddingBottom: 13,
+      color: isValid ? (readonly ? theme.colorTextLight5 : theme.colorTextLight1) : theme.colorError,
+      lineHeight: Platform.OS === 'ios' ? 18 : theme.lineHeight * theme.fontSize,
+      fontSize: theme.fontSize,
+      zIndex: 1,
+      borderRadius: theme.borderRadiusLG,
+    },
+  });
+}
 
 export default React.memo(InputText);
