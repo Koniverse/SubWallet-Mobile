d2d774d9be7f58831402419a0cd8795df7510297
dominhquang <quangdm@subwallet.app>
2023-06-06 18:20:14 +0700

    [Issue-771]: Fix bug can't select token in case change Nominate to Pool when specific account mode

--- src/components/common/StakingTab/index.tsx
+++ src/components/common/StakingTab/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { BUTTON_ACTIVE_OPACITY } from 'constants/index';
 import { useSubWalletTheme } from 'hooks/useSubWalletTheme';
 import StakingTabStyle from './style';
 import { StakingType } from '@subwallet/extension-base/background/KoniTypes';
+import { isEthereumAddress } from '@polkadot/util-crypto';
+import { useSelector } from 'react-redux';
+import { RootState } from 'stores/index';
+import { DisabledStyle } from 'styles/sharedStyles';
 
 interface Props {
   selectedType: StakingType;
   onSelectType: (type: StakingType) => void;
+  from: string;
 }
 
-export const StakingTab = ({ selectedType, onSelectType }: Props) => {
+export const StakingTab = ({ selectedType, onSelectType, from }: Props) => {
+  const currentAccount = useSelector((state: RootState) => state.accountState.currentAccount);
   const theme = useSubWalletTheme().swThemes;
   const _style = StakingTabStyle(theme);
 
@@ -20,11 +26,17 @@ export const StakingTab = ({ selectedType, onSelectType }: Props) => {
     }
   };
 
+  const isDisabled = useMemo(
+    () => isEthereumAddress(currentAccount?.address) && isEthereumAddress(from),
+    [currentAccount?.address, from],
+  );
+
   return (
     <View style={_style.container}>
       <TouchableOpacity
+        disabled={isDisabled}
         activeOpacity={BUTTON_ACTIVE_OPACITY}
-        style={[_style.item, selectedType === StakingType.POOLED && _style.selectedItem]}
+        style={[_style.item, selectedType === StakingType.POOLED && _style.selectedItem, isDisabled && DisabledStyle]}
         onPress={() => _onSelectType(StakingType.POOLED)}>
         <Text style={_style.itemText}>Pools</Text>
       </TouchableOpacity>
