e711060b0035aa0420adf5661ce9fcac3963fe8e
Thuc Nguyen Duy <duythucbk@gmail.com>
2023-07-07 15:48:56 +0700

    lint code browser header

--- src/screens/Home/Browser/Shared/BrowserHeader.tsx
+++ src/screens/Home/Browser/Shared/BrowserHeader.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Header } from 'components/Header';
 import TabIcon from './TabIcon';
+
+const customTabIconStyle = { marginRight: -8 };
 const BrowserTitle = () => {
-  return <Header rightComponent={<TabIcon style={{ marginRight: -8 }} />} />;
+  return <Header rightComponent={<TabIcon style={customTabIconStyle} />} />;
 };
 
 export default BrowserTitle;
