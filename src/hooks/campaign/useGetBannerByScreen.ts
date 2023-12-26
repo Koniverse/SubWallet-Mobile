c410edebd59d18457f9e3b88d9ce61ed9ab14e16
Thuc Nguyen Duy <duythucbk@gmail.com>
2023-12-26 12:19:58 +0700

    [issue-1291] Add flag 'not show again' for MKT Campaign when resetting event iOS 17 is triggered

--- src/hooks/campaign/useGetBannerByScreen.ts
+++ src/hooks/campaign/useGetBannerByScreen.ts
@@ -2,11 +2,17 @@ import { useMemo } from 'react';
 
 import { RootState } from 'stores/index';
 import { useSelector } from 'react-redux';
+import { getBannerSlugs } from 'utils/storage';
 
+const bannerSlugs = getBannerSlugs();
 const useGetBannerByScreen = (screen: string) => {
   const { banners } = useSelector((state: RootState) => state.campaign);
 
   return useMemo(() => {
+    if (banners.length > 0 && bannerSlugs?.includes(banners[0].slug)) {
+      return [];
+    }
+
     return banners.filter(item => item.data.position.includes(screen));
   }, [banners, screen]);
 };
