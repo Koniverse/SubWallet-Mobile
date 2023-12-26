40640284f95dc651de30caa4c777665e2c32f1d7
dominhquang <quangdm@subwallet.app>
2023-12-26 11:43:20 +0700

    [issue-1267] Fix bugs related to Access Camera permission when using WalletConnect feature

--- src/utils/permission/camera.ts
+++ src/utils/permission/camera.ts
@@ -2,9 +2,27 @@ import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
 import { Alert, Linking, Platform } from 'react-native';
 import { AutoLockState } from 'utils/autoLock';
 import i18n from 'utils/i18n/i18n';
+import { mmkvStore } from 'utils/storage';
+export const PERMISSION_STATUS = 'permissionStatus';
 
 const getCameraPermission = () => {
   return Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA;
+};
+
+const showPermissionAlert = (onPressCancel?: () => void) => {
+  Alert.alert(i18n.common.notify, i18n.common.cannotScanQRCodeWithoutPermission, [
+    {
+      text: i18n.buttonTitles.cancel,
+      onPress: onPressCancel,
+    },
+    {
+      text: i18n.common.goToSetting,
+      onPress: () => {
+        onPressCancel && onPressCancel();
+        Linking.openSettings();
+      },
+    },
+  ]);
 };
 
 export const requestCameraPermission = async (onPressCancel?: () => void) => {
@@ -19,26 +37,27 @@ export const requestCameraPermission = async (onPressCancel?: () => void) => {
         // Images: This feature is not available (on this device / in this context)
         break;
       case RESULTS.DENIED:
-        request(getCameraPermission()).then(() => onPressCancel && onPressCancel());
+        Platform.OS === 'ios'
+          ? request(getCameraPermission()).then(() => onPressCancel && onPressCancel())
+          : onPressCancel && onPressCancel();
         // Images: The permission has not been requested / is denied but requestable
         break;
       case RESULTS.GRANTED:
         // Images: The permission is granted
         return result;
       case RESULTS.BLOCKED:
-        Alert.alert(i18n.common.notify, i18n.common.cannotScanQRCodeWithoutPermission, [
-          {
-            text: i18n.buttonTitles.cancel,
-            onPress: onPressCancel,
-          },
-          {
-            text: i18n.common.goToSetting,
-            onPress: () => {
-              onPressCancel && onPressCancel();
-              Linking.openSettings();
-            },
-          },
-        ]);
+        if (Platform.OS === 'ios') {
+          showPermissionAlert(onPressCancel);
+        } else {
+          const permissionStatus = mmkvStore.getString(PERMISSION_STATUS);
+          if (!permissionStatus) {
+            mmkvStore.set(PERMISSION_STATUS, 'blocked');
+            onPressCancel && onPressCancel();
+          } else if (permissionStatus === 'blocked') {
+            showPermissionAlert(onPressCancel);
+          }
+        }
+
         return null;
     }
   } catch (e) {
