71c151b53741b0cc2315a8a28275a30968877dac
dominhquang <quangdm@subwallet.app>
2024-01-03 12:25:07 +0700

    [issue-1261] Add hyperlink when connect Polkadot Vault

--- src/screens/Account/ConnectQrSigner/ConnectParitySigner.tsx
+++ src/screens/Account/ConnectQrSigner/ConnectParitySigner.tsx
@@ -1,10 +1,8 @@
-// Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
 import { ImageLogosMap } from 'assets/logo';
 import React from 'react';
 import ConnectQrSigner from './index';
 import i18n from 'utils/i18n/i18n';
+import { POLKADOT_VAULT_INSTRUCTION_URL } from 'constants/index';
 
 type Props = {};
 
@@ -12,7 +10,7 @@ const ConnectParitySigner: React.FC<Props> = () => {
   return (
     <ConnectQrSigner
       description={i18n.attachAccount.connectPolkadotVaultMessage2}
-      instructionUrl={'Connect your QR wallet'}
+      instructionUrl={POLKADOT_VAULT_INSTRUCTION_URL}
       logoUrl={ImageLogosMap.parity}
       subTitle={i18n.attachAccount.connectPolkadotVaultMessage1}
       title={i18n.header.connectPolkadotVault}
