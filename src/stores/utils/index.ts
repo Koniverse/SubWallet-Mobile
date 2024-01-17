c56b98da9083e198b50c3b9176b84fcec6b5fb13
S2kael <laiducminh1002@gmail.com>
2024-01-17 12:06:22 +0700

    [Issue-1356] Update faq link[Issue-1356] Update web-runner

--- src/stores/utils/index.ts
+++ src/stores/utils/index.ts
@@ -423,7 +423,13 @@ export const subscribeStakingNominatorMetadata = lazySubscribeMessage(
 /* Earning */
 
 export const updateYieldPoolInfo = (data: YieldPoolInfo[]) => {
-  store.dispatch({ type: 'earning/updateYieldPoolInfo', payload: data });
+  addLazy(
+    'updateYieldPoolInfo',
+    () => {
+      store.dispatch({ type: 'earning/updateYieldPoolInfo', payload: data });
+    },
+    900,
+  );
 };
 
 export const subscribeYieldPoolInfo = lazySubscribeMessage(
@@ -434,7 +440,13 @@ export const subscribeYieldPoolInfo = lazySubscribeMessage(
 );
 
 export const updateYieldPositionInfo = (data: YieldPositionInfo[]) => {
-  store.dispatch({ type: 'earning/updateYieldPositionInfo', payload: data });
+  addLazy(
+    'updateYieldPositionInfo',
+    () => {
+      store.dispatch({ type: 'earning/updateYieldPositionInfo', payload: data });
+    },
+    900,
+  );
 };
 
 export const subscribeYieldPositionInfo = lazySubscribeMessage(
@@ -445,7 +457,13 @@ export const subscribeYieldPositionInfo = lazySubscribeMessage(
 );
 
 export const updateYieldReward = (data: EarningRewardJson) => {
-  store.dispatch({ type: 'earning/updateYieldReward', payload: Object.values(data.data) });
+  addLazy(
+    'updateYieldReward',
+    () => {
+      store.dispatch({ type: 'earning/updateYieldReward', payload: Object.values(data.data) });
+    },
+    900,
+  );
 };
 
 export const subscribeYieldReward = lazySubscribeMessage(
