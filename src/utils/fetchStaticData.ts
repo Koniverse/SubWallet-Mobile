2e3ee9e377dc9e689422388d840cc134234cba40
AnhMTV <maithachvietanh@gmail.com>
2024-02-02 17:58:34 +0700

    Quick fix can not fetch network data

--- src/utils/fetchStaticData.ts
+++ src/utils/fetchStaticData.ts
@@ -5,7 +5,7 @@ import { STATIC_DATA_DOMAIN } from 'constants/index';
 export async function fetchStaticData<T>(slug: string, targetFile?: string) {
   const dataByDevModeStatus = getStaticContentByDevMode();
   const fetchFile = targetFile || dataByDevModeStatus;
-  const rs = await axios.get(`${STATIC_DATA_DOMAIN}/${slug}/${fetchFile}`);
+  const rs = await axios.get(`${STATIC_DATA_DOMAIN}/${slug}/${fetchFile}.json`);
 
   return rs.data as T;
 }
