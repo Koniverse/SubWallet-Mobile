fc11bb59f89eff5b7d0804d69fa01095b1bfc620
Thuc Nguyen Duy <duythucbk@gmail.com>
2024-02-16 10:49:17 +0700

    [issue-1469] Can not reset account/eraser all when close app

--- src/screens/MasterPassword/Login/index.tsx
+++ src/screens/MasterPassword/Login/index.tsx
@@ -35,7 +35,7 @@ import {
 import { updateFaceIdEnable, updateUseBiometric } from 'stores/MobileSettings';
 import { FORCE_HIDDEN_EVENT } from 'components/design-system-ui/modal/ModalBaseV2';
 import MigrateToKeychainPasswordModal from '../MigrateToKeychainPasswordModal';
-import { backupStorageData, mmkvStore } from 'utils/storage';
+import { clearBackupData, mmkvStore } from 'utils/storage';
 import { setBuildNumber } from 'stores/AppVersion';
 import { LockTimeout } from 'stores/types';
 import useConfirmationsInfo from 'hooks/screen/Confirmation/useConfirmationsInfo';
@@ -217,7 +217,6 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
         _setLoading(true);
 
         setTimeout(() => {
-          _setLoading(false);
           resetWallet({
             resetAll: resetAll,
           })
@@ -228,9 +227,9 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
             })
             .catch((e: Error) => {
               toast.show(e.message, { type: 'danger' });
+              _setLoading(false);
             })
             .finally(() => {
-              _setLoading(false);
               setModalVisible(false);
               if (resetAll) {
                 resetPinCode();
@@ -238,8 +237,8 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
                 dispatch(updateUseBiometric(false));
                 resetKeychainPassword();
               }
-              // BACKUP-003: Back up local storage after reset account
-              backupStorageData(true, false);
+              // Clear backed up data after reset account
+              clearBackupData();
             });
         }, 300);
       };
