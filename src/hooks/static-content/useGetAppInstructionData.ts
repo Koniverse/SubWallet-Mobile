8f4b17080b5d92959bb2dd43bc9c694102d539af
dominhquang <quangdm@subwallet.app>
2024-06-06 12:06:51 +0700

    Add dataByDevMode status for Instruction mkt campaign

--- src/hooks/static-content/useGetAppInstructionData.ts
+++ src/hooks/static-content/useGetAppInstructionData.ts
@@ -1,14 +1,14 @@
 import { useCallback } from 'react';
 import axios from 'axios';
 import { STATIC_DATA_DOMAIN } from 'constants/index';
-import { mmkvStore } from 'utils/storage';
+import { getStaticContentByDevMode, mmkvStore } from 'utils/storage';
 
-// const dataByDevModeStatus = getStaticContentByDevMode(); add this when data is ready
+const dataByDevModeStatus = getStaticContentByDevMode();
 
 export const useGetAppInstructionData = (language: string) => {
   const getAppInstructionData = useCallback(async () => {
     axios
-      .get(`${STATIC_DATA_DOMAIN}/instructions/preview-${language}.json`)
+      .get(`${STATIC_DATA_DOMAIN}/instructions/${dataByDevModeStatus}-${language}.json`)
       .then(({ data }) => {
         try {
           mmkvStore.set('app-instruction-data', JSON.stringify(data));
