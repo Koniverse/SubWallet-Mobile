0b1c95a31c552482dbfd9e37fe304c27e9575302
dominhquang <quangdm@subwallet.app>
2024-07-04 15:20:14 +0700

    [Issue-1546] Add some features of marketing campain ( Phrase 3)

--- src/hooks/static-content/useHandleAppBannerMap.ts
+++ src/hooks/static-content/useHandleAppBannerMap.ts
@@ -1,24 +1,53 @@
 import { useCallback, useMemo } from 'react';
-import {
-  AppBannerData,
-  AppBasicInfoData,
-  ConditionBalanceType,
-  ConditionEarningType,
-  PopupHistoryData,
-} from 'types/staticContent';
+import { AppBannerData, AppBasicInfoData, PopupHistoryData } from 'types/staticContent';
 import { updateAppBannerData, updateBannerHistoryData } from 'stores/base/StaticContent';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { YieldPositionInfo } from '@subwallet/extension-base/types';
+import { RootState } from 'stores/index';
+import BigN from 'bignumber.js';
+import { _getAssetDecimals } from '@subwallet/extension-base/services/chain-service/utils';
+import { getOutputValuesFromString } from 'components/Input/InputAmount';
+import { BN_ZERO } from 'utils/chainBalances';
+import useFetchNftCollection from 'hooks/screen/Home/Nft/useFetchNftCollection';
 
 export const useHandleAppBannerMap = (
-  appBannerData: AppBannerData[],
-  bannerHistoryMap: Record<string, PopupHistoryData>,
   yieldPositionList: YieldPositionInfo[],
   checkPopupExistTime: (info: AppBasicInfoData) => boolean,
-  checkBalanceCondition: (conditionBalance: ConditionBalanceType[]) => boolean,
-  checkEarningCondition: (_yieldPositionList: YieldPositionInfo[], conditionEarning: ConditionEarningType[]) => boolean,
 ) => {
   const dispatch = useDispatch();
+  const { appBannerData, bannerHistoryMap } = useSelector((state: RootState) => state.staticContent);
+  const { assetRegistry } = useSelector((state: RootState) => state.assetRegistry);
+  const { balanceMap } = useSelector((state: RootState) => state.balance);
+  const { chainInfoMap } = useSelector((state: RootState) => state.chainStore);
+  const { nftCollections } = useFetchNftCollection();
+
+  const hasMoneyArr = useMemo(() => {
+    let result: string[] = [];
+
+    if (balanceMap.ALL) {
+      const isHasBalance = Object.values(balanceMap.ALL).find(item => {
+        const freeBalance = item?.free;
+        const lockedBalance = item?.locked;
+        const value = new BigN(freeBalance).plus(lockedBalance);
+
+        return value.gt(BN_ZERO);
+      });
+
+      if (isHasBalance) {
+        result.push('balance');
+      }
+    }
+
+    if (nftCollections && nftCollections.length) {
+      result.push('nft');
+    }
+
+    if (yieldPositionList && yieldPositionList.length) {
+      result.push('earning');
+    }
+
+    return result;
+  }, [balanceMap, nftCollections, yieldPositionList]);
 
   const getFilteredAppBannerByTimeAndPlatform = useCallback(
     (data: AppBannerData[]) => {
@@ -73,22 +102,103 @@ export const useHandleAppBannerMap = (
     [bannerHistoryMap, dispatch],
   );
 
-  const filteredAppBannerMap = useMemo(() => {
-    return appBannerData.filter(item => {
-      if (!!Object.keys(item.conditions) && !!Object.keys(item.conditions).length) {
-        const isPassBalanceCondition = item.conditions['condition-balance']
-          ? checkBalanceCondition(item.conditions['condition-balance'])
-          : true;
+  const checkComparison = useCallback((comparison: string, value: string, comparisonValue: string) => {
+    switch (comparison) {
+      case 'eq':
+        return new BigN(value).eq(comparisonValue);
+      case 'gt':
+        return new BigN(value).gt(comparisonValue);
+      case 'gte':
+        return new BigN(value).gte(comparisonValue);
+      case 'lt':
+        return new BigN(value).lt(comparisonValue);
+      case 'lte':
+        return new BigN(value).lte(comparisonValue);
+      default:
+        return true;
+    }
+  }, []);
 
-        const isPassEarningCondition = item.conditions['condition-earning']
-          ? checkEarningCondition(yieldPositionList, item.conditions['condition-earning'])
-          : true;
-        return isPassBalanceCondition || isPassEarningCondition;
+  const filteredAppBannerMap = useMemo(() => {
+    return appBannerData?.filter(item => {
+      if (!!Object.keys(item.conditions) && !!Object.keys(item.conditions).length) {
+        const isPassValidation: boolean[] = [];
+
+        if (item.conditions['condition-balance'] && item.conditions['condition-balance'].length) {
+          const dataFilterByBalanceCondition = item.conditions['condition-balance'].map(_item => {
+            return Object.values(balanceMap).some(info => {
+              const balanceData = info[_item.chain_asset];
+              const decimals = _getAssetDecimals(assetRegistry[_item.chain_asset]);
+              const freeBalance = balanceData?.free;
+              const lockedBalance = balanceData?.locked;
+              const value = new BigN(freeBalance).plus(lockedBalance).toString();
+              const comparisonValue = getOutputValuesFromString(_item.value.toString(), decimals);
+
+              return checkComparison(_item.comparison, value, comparisonValue);
+            });
+          });
+
+          isPassValidation.push(dataFilterByBalanceCondition.some(d => d));
+        }
+
+        if (item.conditions['condition-earning'] && item.conditions['condition-earning'].length) {
+          const dataFilterByEarningCondition = item.conditions['condition-earning'].map(condition => {
+            const yieldPosition = yieldPositionList.find(_item => _item.slug === condition.pool_slug);
+
+            if (yieldPosition) {
+              const chainInfo = chainInfoMap[yieldPosition.chain];
+              const decimals = chainInfo?.substrateInfo?.decimals || chainInfo?.evmInfo?.decimals;
+              const activeStake = yieldPosition.totalStake;
+              const comparisonValue = getOutputValuesFromString(condition.value.toString(), decimals || 0);
+
+              return checkComparison(condition.comparison, activeStake, comparisonValue);
+            } else {
+              return false;
+            }
+          });
+
+          isPassValidation.push(dataFilterByEarningCondition.some(i => i));
+        }
+
+        if (item.conditions['condition-nft'] && item.conditions['condition-nft'].length) {
+          const dataFilterByNftCondition = nftCollections.find(nft =>
+            item.conditions['condition-nft'].find(
+              cond => cond.chain === nft.chain || cond.collection_id === nft.collectionId,
+            ),
+          );
+
+          isPassValidation.push(!!dataFilterByNftCondition);
+        }
+
+        if (
+          item.conditions['condition-has-money'] &&
+          item.conditions['condition-has-money'][0] &&
+          item.conditions['condition-has-money'][0].has_money
+        ) {
+          const isHasMoney = item.conditions['condition-has-money'][0].has_money.some(i => hasMoneyArr.includes(i));
+
+          isPassValidation.push(isHasMoney);
+        }
+
+        if (isPassValidation && isPassValidation.length) {
+          return isPassValidation.some(_i => _i);
+        } else {
+          return true;
+        }
       } else {
         return true;
       }
     });
-  }, [appBannerData, checkBalanceCondition, checkEarningCondition, yieldPositionList]);
+  }, [
+    appBannerData,
+    assetRegistry,
+    balanceMap,
+    chainInfoMap,
+    checkComparison,
+    hasMoneyArr,
+    nftCollections,
+    yieldPositionList,
+  ]);
 
   const appBannerMap = useMemo(() => {
     if (filteredAppBannerMap) {
