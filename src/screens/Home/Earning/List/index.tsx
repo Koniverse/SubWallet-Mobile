abcf0a076410daecae840b4bf5099140081b6af3
dominhquang <quangdm@subwallet.app>
2024-08-22 19:41:25 +0700

    [Issue-1769] Notice of need to unstake for users who are simultaneously staking for both Direct and Nomination Pool

--- src/screens/Home/Earning/List/index.tsx
+++ src/screens/Home/Earning/List/index.tsx
@@ -4,10 +4,21 @@ import PositionList from 'screens/Home/Earning/PositionList';
 import GroupList from 'screens/Home/Earning/GroupList';
 import { EarningListProps } from 'routes/earning';
 import { Alert } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'stores/index';
-import { useNavigation } from '@react-navigation/native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { RootNavigationProps } from 'routes/index';
+import { mmkvStore } from 'utils/storage';
+import BigN from 'bignumber.js';
+import { updateMktCampaignStatus } from 'stores/AppState';
+import WarningModal from 'components/Modal/WarningModal';
+
+const NEED_CHECK_TO_SHOW_WARNING_SLUGS = [
+  'DOT___nomination_pool___polkadot',
+  'DOT___native_staking___polkadot',
+  'KSM___nomination_pool___kusama',
+  'KSM___native_staking___kusama',
+];
 
 let isShowAlert = false;
 export const EarningList = ({
@@ -23,8 +34,51 @@ export const EarningList = ({
   const { chainInfoMap } = useSelector((state: RootState) => state.chainStore);
   const [currentStep, setCurrentStep] = useState(step || 1);
   const [firstLoading, setFirstLoading] = useState(true);
+  const [warningModalVisible, setWarningModalVisible] = useState<boolean>(false);
   const [positionLoading, setPositionLoading] = useState(false);
   const rootNavigation = useNavigation<RootNavigationProps>();
+  const isOpenedWarningPopup = mmkvStore.getBoolean('isOpenedWarningPopup');
+  const { yieldPositions } = useSelector((state: RootState) => state.earning);
+  const dispatch = useDispatch();
+  const isFocused = useIsFocused();
+
+  const isShowWarningPopup = useMemo(() => {
+    const filteredYieldPositionList = yieldPositions.filter(y => {
+      const haveStake = new BigN(y.totalStake).gt(0);
+      return NEED_CHECK_TO_SHOW_WARNING_SLUGS.includes(y.slug) && haveStake;
+    });
+    let filteredYieldPositionMapByAddress: Record<string, string[]> = {};
+
+    filteredYieldPositionList.forEach(obj => {
+      if (filteredYieldPositionMapByAddress[obj.address]) {
+        filteredYieldPositionMapByAddress[obj.address].push(obj.slug);
+      } else {
+        filteredYieldPositionMapByAddress[obj.address] = [obj.slug];
+      }
+    });
+
+    return Object.keys(filteredYieldPositionMapByAddress).some(address => {
+      const positionSlugs = filteredYieldPositionMapByAddress[address];
+      const showDotWarningPopup =
+        positionSlugs.includes('DOT___nomination_pool___polkadot') &&
+        positionSlugs.includes('DOT___native_staking___polkadot');
+
+      const showKsmWarningPopup =
+        positionSlugs.includes('KSM___nomination_pool___kusama') &&
+        positionSlugs.includes('KSM___native_staking___kusama');
+      return showDotWarningPopup || showKsmWarningPopup;
+    });
+  }, [yieldPositions]);
+
+  // TODO: Remove this later because this is a hot fix for mkt campaign
+  useEffect(() => {
+    if (isShowWarningPopup && !isOpenedWarningPopup && isFocused) {
+      setWarningModalVisible(true);
+      dispatch(updateMktCampaignStatus(false));
+    } else {
+      dispatch(updateMktCampaignStatus(true));
+    }
+  }, [dispatch, isFocused, isOpenedWarningPopup, isShowWarningPopup]);
 
   const chainName = useMemo(() => {
     if (chain) {
@@ -104,6 +158,15 @@ export const EarningList = ({
       ) : (
         <GroupList isHasAnyPosition={!!data.length} setStep={setCurrentStep} />
       )}
+
+      <WarningModal
+        visible={warningModalVisible}
+        setVisible={setWarningModalVisible}
+        onPressBtn={() => {
+          mmkvStore.set('isOpenedWarningPopup', true);
+          setWarningModalVisible(false);
+        }}
+      />
     </>
   );
 };
