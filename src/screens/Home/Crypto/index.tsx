781663ea79781e84894bcb4f137f275dba209f7a
dominhquang <quangdm@subwallet.app>
2024-09-05 15:45:54 +0700

    [Issue-1781] Update web-runner v1.2.28

--- src/screens/Home/Crypto/index.tsx
+++ src/screens/Home/Crypto/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { ComponentType } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TokenGroups } from 'screens/Home/Crypto/TokenGroups';
 import { TokenGroupsDetail } from 'screens/Home/Crypto/TokenGroupsDetail';
 import { CryptoStackParamList } from 'routes/home';
+import withPageWrapper from 'components/pageWrapper';
+
+const TokenGroupsDetailScreen = (props: JSX.IntrinsicAttributes) => {
+  return withPageWrapper(TokenGroupsDetail as ComponentType, ['swap'])(props);
+};
 
 export const CryptoScreen = () => {
   const Stack = createNativeStackNavigator<CryptoStackParamList>();
@@ -10,7 +15,7 @@ export const CryptoScreen = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
       <Stack.Screen name="TokenGroups" component={TokenGroups} />
-      <Stack.Screen name="TokenGroupsDetail" component={TokenGroupsDetail} />
+      <Stack.Screen name="TokenGroupsDetail" component={TokenGroupsDetailScreen} />
     </Stack.Navigator>
   );
 };
