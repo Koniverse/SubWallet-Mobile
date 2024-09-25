47d5e54560f298feb062cea2b4e5e057c0a7a7e5
dominhquang <quangdm@subwallet.app>
2024-09-25 11:23:34 +0700

    [Issue-1780] Fix bug crash app on real device

--- android/app/src/main/java/app/subwallet/mobile/MainApplication.kt
+++ android/app/src/main/java/app/subwallet/mobile/MainApplication.kt
@@ -11,6 +11,12 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.os.Build;
+import org.jetbrains.annotations.Nullable;
 
 import app.subwallet.mobile.nativeModules.RCTAppInstallerPackage
 import app.subwallet.mobile.nativeModules.RCTMinimizerPackage
@@ -38,6 +44,14 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun registerReceiver(receiver: BroadcastReceiver?, filter: IntentFilter): Intent? {
+    return if (Build.VERSION.SDK_INT >= 34 && applicationInfo.targetSdkVersion >= 34) {
+      super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED)
+    } else {
+      super.registerReceiver(receiver, filter)
+    }
+  }
 
   override fun onCreate() {
     super.onCreate()
